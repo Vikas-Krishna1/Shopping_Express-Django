e91585a3104dd170c19f46edbd5ed63ed06d454c
Vikas Krishna <vikas.krishna200007@gmail.com>
2026-02-06 13:41:10 -0500

    Railway deploy ready

--- orders/admin.py
+++ orders/admin.py
@@ -3,12 +3,15 @@ from .models import *
 from accounts.models import *
 from items.models import *
 from cart.models import *
+from accounts.models import *
+
 
 # Register your models here.
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(CartItem)
 admin.site.register(Item)
-admin.site.register(Profile)
 admin.site.register(Address)
+
+
 
